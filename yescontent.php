aaf597fa47a1eb2b129a7d334a88e0fb21082f95
Axel Paris <axel.paris@deux-trente.com>
2023-02-09 09:51:24 +0100

    fix

--- yescontent.php
+++ yescontent.php
@@ -16,7 +16,7 @@
  * Plugin Name:       YesContent
  * Plugin URI:        https://yescontent.rocks
  * Description:       Plateforme de gestion et publication de contenu
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Axel Paris
  * Author URI:        https://yescontent.rocks
  * License:           GPL-2.0+
